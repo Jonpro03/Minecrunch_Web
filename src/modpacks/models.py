12085f3e1cfad146f50315bb2e3794b48a64825c
Windigo <jacob@fragdev.com>
2016-09-10 16:54:05 -0700

    Modifying the app models

--- src/modpacks/models.py
+++ src/modpacks/models.py
@@ -8,16 +8,33 @@ class Mod(models.Model):
     A mod that can be combined to make a modpack
     """
 
-    desc = models.CharField(max_length=99)
-    version = models.CharField(max_length=20)
-    author = models.CharField(max_length=99)
+    mod_name = models.CharField(
+        max_length=99,
+        verbose_name='Mod Name'
+    )
+    desc = models.TextField(
+        verbose_name='Mod Description'
+    )
+    version = models.CharField(
+        max_length=20,
+        verbose_name='Mod Version'
+    )
+    author = models.CharField(max_length=99, verbose_name='Mod Author')
 
 
 class Modpack(models.Model):
     """
     A combination of mods that can be run on a server/client
     """
-    
-    desc = models.CharField(max_length=99)
-    #image = # TODO: Image upload stuff
-    mods = models.ManyToManyField(Mod, blank=True)    
+
+    modpack_name = models.CharField(
+        max_length=99,
+        verbose_name='Mod Author'
+    )
+    desc = models.TextField(verbose_name='Mod Author')
+    # image = # TODO: Image upload stuff?
+    mods = models.ManyToManyField(
+        Mod,
+        blank=True,
+        verbose_name='Mod Author'
+    )
