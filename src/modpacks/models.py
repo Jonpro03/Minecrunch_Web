d4bf7041bc8ac057f38a66245ed0ac5df12ff17a
Windigo <jacob@fragdev.com>
2016-09-12 11:20:20 -0700

    Fixed syntax errors and created initial migration

--- src/modpacks/models.py
+++ src/modpacks/models.py
@@ -6,9 +6,12 @@ class Mod(models.Model):
     """
     A mod that can be combined to make a modpack
     """
-    mod_name = models.CharField(verbose_name='Mod name')
-    version = models.CharField(verbose_name='Mod version')
-    author = models.CharField(verbose_name='Mod author')
+    mod_name = models.CharField(verbose_name='Mod name',
+                                max_length=200)
+    version = models.CharField(verbose_name='Mod version',
+                               max_length=10)
+    author = models.CharField(verbose_name='Mod author',
+                              max_length=200)
     url = models.URLField(verbose_name='Link to mod website',
                           blank=True)
 
@@ -17,7 +20,8 @@ class Modpack(models.Model):
     """
     A combination of mods that can be run on a server/client
     """
-    modpack_name = models.CharField(verbose_name='Modpack Name')
+    modpack_name = models.CharField(verbose_name='Modpack Name',
+                                    max_length=200)
     desc = models.TextField(verbose_name='Description of modpack',
                             blank=True)
     # image = # TODO: Image upload stuff?
--- src/modpacks/urls.py
+++ src/modpacks/urls.py
@@ -5,6 +5,6 @@ from django.conf.urls import url
 from modpacks import views
 
 urlpatterns = [
-    url(r'^/', views.index),
+    url(r'^$', views.index),
     url(r'^(?P<slug>.+)/', views.modpack),
 ]
