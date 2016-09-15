ee4f787a7975654b3ce40c2839ef531be8dcbd8d
Windigo <jacob@fragdev.com>
2016-09-15 13:31:37 -0700

    Create whitelist models to keep track of players

--- src/whitelist/models.py
+++ src/whitelist/models.py
@@ -2,4 +2,21 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Player(models.Model):
+    """A minecraft player, and their current whitelist status on the server
+    """
+
+    APPROVED = 'y'
+    DENIED = 'n'
+    WHITELIST_STATUS = (
+        (APPROVED, 'Approved'),
+        (DENIED, 'Denied'),
+        )
+
+    ign = models.CharField(verbose_name="In-Game Account Name",
+                           max_length=50)
+    status = models.CharField(verbose_name="Player Status",
+                              max_length=1,
+                              blank=True)
+    email = models.EmailField(verbose_name="Email Address")
--- src/whitelist/views.py
+++ src/whitelist/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
+
+def index(request):
+    """Whitelist Request
+    
+    Explains the server whitelist policy, and provides a form for applying to be whitelisted
+    """
+
+    template = loader.get_template('home/home.html')
+
+    if request.method == 'POST':
+        pass
+
+    return HttpResponse(template.render())
