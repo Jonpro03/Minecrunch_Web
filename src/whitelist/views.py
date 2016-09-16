de8e5b4790200da105fb4f8b971ed3ffd9a208bd
Windigo <jacob@fragdev.com>
2016-09-16 14:59:03 -0700

    Added whitelist request view

--- src/whitelist/views.py
+++ src/whitelist/views.py
@@ -1,16 +1,26 @@
 from django.http import HttpResponse
 from django.template import loader
+from .models import Player
+from django.shortcuts import redirect
+from django.contrib.sites import requests
+from .whitelist_form import WhitelistForm
 
 
 def index(request):
     """Whitelist Request
-    
-    Explains the server whitelist policy, and provides a form for applying to be whitelisted
+
+    Explains the server whitelist policy, and provides a form for applying to
+    be whitelisted
     """
 
-    template = loader.get_template('home/home.html')
+    player = Player()
+    form = WhitelistForm(request.POST, instance=player)
 
-    if request.method == 'POST':
-        pass
+    # Handle a request to be whitelisted
+    if request.method == 'POST' and form.is_valid():
+        player.save()
+        return redirect('whitelist:submitted')
 
-    return HttpResponse(template.render())
+    template = loader.get_template('whitelist/index.html')
+
+    return HttpResponse(template.render({'form': form}))
