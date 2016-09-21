abc69ed7a96051e4a081436e687741a9203ccbc4
Windigo <jacob@fragdev.com>
2016-09-21 11:20:05 -0700

    Moved WhitelistForm class into a "util" module, changed it to use bootstrap classes on form elements

---
+++ src/whitelist/util/whitelist_form.py
@@ -0,0 +1,14 @@
+from django.forms import ModelForm, TextInput
+from whitelist.models import Player
+
+
+class WhitelistForm(ModelForm):
+    """ Automatically generate a form based on the Player model
+    """
+    class Meta:
+        model = Player
+        exclude = ('status',)
+        widgets = {
+            'ign': TextInput(attrs={'class': 'form-control'}),
+            'email': TextInput(attrs={'class': 'form-control'}),
+        }
