d54b833450b433fefede2a1fdaab76171a9aa8a0
Windigo <jacob@fragdev.com>
2016-09-21 11:21:01 -0700

    Changed whitelist request view to properly handle form submissions, and the render call to include the request (for CSRF value)

--- src/whitelist/views.py
+++ src/whitelist/views.py
@@ -2,8 +2,7 @@ from django.http import HttpResponse
 from django.template import loader
 from .models import Player
 from django.shortcuts import redirect
-from django.contrib.sites import requests
-from .whitelist_form import WhitelistForm
+from .util.whitelist_form import WhitelistForm
 
 
 def index(request):
@@ -14,13 +13,17 @@ def index(request):
     """
 
     player = Player()
-    form = WhitelistForm(request.POST, instance=player)
 
     # Handle a request to be whitelisted
-    if request.method == 'POST' and form.is_valid():
-        player.save()
-        return redirect('whitelist:submitted')
+    if request.method == 'POST':
+        form = WhitelistForm(request.POST, instance=player)
+
+        if form.is_valid():
+            player.save()
+            return redirect('submitted')
+    else:
+        form = WhitelistForm()
 
     template = loader.get_template('whitelist/index.html')
 
-    return HttpResponse(template.render({'form': form}))
+    return HttpResponse(template.render({'form': form}, request))
