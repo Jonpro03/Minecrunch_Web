52364b29e26584a4af3f6b0ea39625f6616b1c91
Windigo <jacob@fragdev.com>
2016-09-27 15:24:48 -0700

    Broke admin section into three player states

--- src/whitelist/admin.py
+++ src/whitelist/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
-from .models import Player
+from .util.approved_admin import ApprovedAdmin
+from .models.approved_player import ApprovedPlayer
+from .util.denied_admin import DeniedAdmin
+from .models.denied_player import DeniedPlayer
+from .util.pending_admin import PendingAdmin
+from .models.pending_player import PendingPlayer
 
-admin.site.register(Player)
+admin.site.register(ApprovedPlayer, ApprovedAdmin)
+admin.site.register(DeniedPlayer, DeniedAdmin)
+admin.site.register(PendingPlayer, PendingAdmin)
