eb3cfd84d8b4f7d3a9a4ff148900c89acd40c954
Windigo <jacob@fragdev.com>
2016-09-28 14:23:11 -0700

    Added UUID field to the Player model for the Mojang UUID

--- src/whitelist/models/player.py
+++ src/whitelist/models/player.py
@@ -1,5 +1,7 @@
 from __future__ import unicode_literals
 from django.db import models
+from urllib2 import urlopen, HTTPError, URLError
+import json
 
 
 class Player(models.Model):
@@ -13,16 +15,42 @@ class Player(models.Model):
         (APPROVED, 'Approved'),
         (DENIED, 'Denied'),
         (PENDING, 'Pending'),)
+    BLANK_UUID = '00000000000000000000000000000000'
 
-    ign = models.CharField("In-Game Account Name",
+    ign = models.CharField("in-game account name",
                            max_length=50,
                            unique=True)
-    status = models.CharField("Player Status",
+    status = models.CharField("player status",
                               max_length=1,
                               choices=WHITELIST_STATUS,
                               default=PENDING)
-    email = models.EmailField("Email Address",
+    email = models.EmailField("email address",
                               unique=True)
+    uuid = models.UUIDField("Mojang UUID",
+                            default=BLANK_UUID)
 
     def __str__(self):
         return self.ign
+
+    def fetch_account_details(self):
+        url = 'https://api.mojang.com/users/profiles/minecraft/{}'
+        url = url.format(self.ign)
+
+        try:
+            response = urlopen(url)
+            player_data = json.load(response)
+
+            # Overwrite the player's provided name with the formatted version
+            self.ign = player_data[u'name']
+            self.uuid = player_data[u'id']
+
+        except HTTPError, URLError:
+            pass
+
+    def save(self):
+        if self.uuid.hex == self.BLANK_UUID:
+            self.fetch_account_details()
+
+        super(Player, self).save()
+
+        return self
