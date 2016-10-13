89d9f160d737eb6f219bfe2d969c3f938a0c4901
Windigo <jacob@fragdev.com>
2016-10-13 13:44:45 -0700

    Remove older modpack reference from home app, and set up home app as the homepage

--- src/MinecrunchWeb/urls.py
+++ src/MinecrunchWeb/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^home/', include('home.urls')),
+    url(r'^$', include('home.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^modpacks/', include('modpacks.urls')),
     url(r'^whitelist/', include('whitelist.urls')),
