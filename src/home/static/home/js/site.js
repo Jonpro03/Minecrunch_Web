58143382ab271852052402fcc8916e7ba7a7ed5e
JonProietti <jon.proietti@watchfiresigns.com>
2016-12-30 10:23:43 -0600

    Added head spinner

--- src/home/static/home/js/site.js
+++ src/home/static/home/js/site.js
@@ -1,18 +1,28 @@
-﻿
-$("#menu-toggle").click(function(e) {
-        e.preventDefault();
-        $("#wrapper").toggleClass("active");
+﻿$("#menu-toggle").click(function(e) {
+	e.preventDefault();
+	$("#wrapper").toggleClass("active");
 });
 
-$('a[href*="#"]:not([href="#"])').click(function() {
-	  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-	    var target = $(this.hash);
-	    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-	    if (target.length) {
-	      $('html, body').animate({
-	        scrollTop: target.offset().top
-	      }, 1000);
-	      return false;
-	    }
+$('a[href*="#"]:not([href="#"])').click(
+		function() {
+			if (location.pathname.replace(/^\//, '') == this.pathname.replace(
+					/^\//, '')
+					&& location.hostname == this.hostname) {
+				var target = $(this.hash);
+				target = target.length ? target : $('[name='
+						+ this.hash.slice(1) + ']');
+				if (target.length) {
+					$('html, body').animate({
+						scrollTop : target.offset().top
+					}, 1000);
+					return false;
+				}
+			}
+		});
+
+$(document).ready(function(){
+	if (navigator.userAgent.match(/msie|trident/i))
+	  {
+	    $(".cube").hide();
 	  }
-	});
+	}); 
